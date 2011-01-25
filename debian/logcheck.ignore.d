9d19c9f9dbe0384e1cf8b0e46a5dc3c71e9d2572
Rainer Clasen <bj@zuto.de>
2011-01-25 09:56:02 +0100

    removed obsolete logcheck file

--- debian/logcheck.ignore.d
+++
@@ -1,6 +0,0 @@
-^\w{3} [ :0-9]{11} [._[:alnum:]-]+ nspamper\[[0-9]+\]: old IP: [0-9.]+$
-^\w{3} [ :0-9]{11} [._[:alnum:]-]+ nspamper\[[0-9]+\]: current IP: [0-9.]+$
-^\w{3} [ :0-9]{11} [._[:alnum:]-]+ nspamper\[[0-9]+\]: new IP: [0-9.]+$
-^\w{3} [ :0-9]{11} [._[:alnum:]-]+ nspamper\[[0-9]+\]: waiting for IP change$
-^\w{3} [ :0-9]{11} [._[:alnum:]-]+ nspamper\[[0-9]+\]: remembering new IP$
-^\w{3} [ :0-9]{11} [._[:alnum:]-]+ nspamper\[[0-9]+\]: updating .*=[0-9.]+$
